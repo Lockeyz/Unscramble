9a7f404b4110f990ca3cd0d541db9584a399e950
ducloc <locdivine@gmail.com>
2024-07-11 13:50:51 +0700

    using ViewModel and LiveData

--- app/src/main/java/com/example/android/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/android/unscramble/MainActivity.kt
@@ -23,7 +23,6 @@ import androidx.appcompat.app.AppCompatActivity
  * Creates an Activity that hosts the Game fragment in the app
  */
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
